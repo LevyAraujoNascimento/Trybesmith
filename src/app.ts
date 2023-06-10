fdd9606cdd42f8926c0b6d66f6734b0a8bcd2a16
Levy Araujo <levy4raujo@gmail.com>
2023-06-10 12:50:06 -0300

    [Req 5 Concluído // Falta Teste]
    Validando dados de entrada.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express'; // Iniciando
 import productController from './database/controllers/product.controller';
 import orderController from './database/controllers/order.controller';
 import userController from './database/controllers/user.controller';
+import validName from './database/middlewares/nameValidation';
+import validPrice from './database/middlewares/priceValidation';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/products', productController.newProduct);
+app.post('/products', validName, validPrice, productController.newProduct);
 
 app.get('/products', productController.listAll);
 
