1e44784992fad87af8999d7a59cef294bbd4ac04
Levy Araujo <levy4raujo@gmail.com>
2023-06-12 16:09:06 -0300

    [TDD Teste 3 Coverage]
    Aumentando o Coverage.

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,6 +3,9 @@ import orderService from '../services/order.service';
 
 const listAll = async (_req: Request, res: Response): Promise<Response> => {
   const orders = await orderService.listAll();
+  if (!orders) {
+    return res.status(430).end();  
+  }
   return res.status(200).send(orders);  
 };
 
